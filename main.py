83b7b1e58910163cab2f1102f81d76a82824d283
Ezequiel Capaceta <ezequielcapaceta@zeki.local>
2023-02-26 12:38:22 -0800

    finished problem 2

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ print(sorted_grades)
 
 # Problem 2: Write a lambda function to cube every element of a list
 
+cubed = lambda x: [i**3 for i in x]
+print(cubed([3,6,9,2]))
+
 # Problem 3: Write a lambda function to determine whether a number is even 
 # or odd (the function should return True or False), and then use the 
 # function and a list comprehension to create a new list of booleans, 
