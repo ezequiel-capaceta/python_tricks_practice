a84c08b34bb8be3902d05ed6edb3afe82ce4e0b1
Ezequiel Capaceta <ezequielcapaceta@zeki.local>
2023-02-26 12:36:52 -0800

    Added instructions for all 5 problems

---
+++ main.py
@@ -0,0 +1,15 @@
+# Problem 1: Write a lambda function to sort a list of tuples in ascending 
+# order according to the number in the second position of each tuple.
+
+# Problem 2: Write a lambda function to cube every element of a list
+
+# Problem 3: Write a lambda function to determine whether a number is even 
+# or odd (the function should return True or False), and then use the 
+# function and a list comprehension to create a new list of booleans, 
+# where even numbers are True and odd numbers are False.
+
+# Problem 4: Use a list comprehension to create a list of the numbers from 
+# 1 to 100 (including 100).
+
+# Problem 5: Use a dictionary comprehension to count the length of each 
+# word in a sentence.
